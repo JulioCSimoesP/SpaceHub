304eb6a4421936b4f39e7a3d76864f9e5f95a59a
JulioCSimoesP <juliocsimoesp@gmail.com>
2026-07-10 23:41:14 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,23 @@
+/* App initialization */
+
+const AppState = {
+    name: ''
+};
+
+const AppController = {
+    initialize() {
+        
+    }
+};
+
+window.addEventListener('DOMContentLoaded', () => AppController.initialize());
+
+/* Static assets */
+
+const CATEGORY_ICON_LIBRARY = [
+    { id: "", name: "", svg: `` },
+];
+
+const UTILITY_ICON_MAP = {
+    name: ``,
+};
